faf1f6631ff7a6368b9be84b3d752e2073aac52b
Alister Animesh Baroi <Alister.Baroi@gmail.com>
2023-05-20 02:27:41 +0800

    fix forder issue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '../imagineHackVue/src/App.vue'
+import router from '../imagineHackVue/src/router'
 
 const app = createApp(App)
 
